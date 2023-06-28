e0bf002a93a02020283fc700637e1d74acf3fcd9
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-06-28 10:04:13 -0400

    Java String concepts

---
+++ Strings/src/PrettyPrinting.java
@@ -0,0 +1,9 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted Number is %.2f \n", a);
+        System.out.printf("Value of pie: %.3f \n", Math.PI);
+        System.out.printf("Hello my name is %s and I am %s \n", "Srinivas", "cool");
+
+    }
+}
