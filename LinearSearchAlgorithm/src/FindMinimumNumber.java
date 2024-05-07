b09fd5549d9cd64f407b0ee38ab51e861223fe7a
Srinivasa Reddy Akumalla <113478192+akumalla-s@users.noreply.github.com>
2024-05-06 21:52:50 -0400

    Adding Linear Search Algorithm java project

---
+++ LinearSearchAlgorithm/src/FindMinimumNumber.java
@@ -0,0 +1,19 @@
+public class FindMinimumNumber {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, -7, 3, 14, 28};
+        System.out.println(min(arr));
+    }
+
+    //Assume array is not empty
+    private static int min(int[] arr) {
+        int ans = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+
+}
