f0118dbaeec358d95a08107d6d86e026dbf2aa77
akumalla-s <akumalla.s@northeastern.edu>
2024-05-17 14:03:22 -0400

    removed files

--- Strings/src/PrettyPrinting.java
+++
@@ -1,9 +0,0 @@
-public class PrettyPrinting {
-    public static void main(String[] args) {
-        float a = 453.1274f;
-        System.out.printf("Formatted Number is %.2f \n", a);
-        System.out.printf("Value of pie: %.3f \n", Math.PI);
-        System.out.printf("Hello my name is %s and I am %s \n", "Srinivas", "cool");
-
-    }
-}
