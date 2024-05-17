0b1a7fdc225fd1e2369c17bc93a929f2297b8e95
akumalla-s <akumalla.s@northeastern.edu>
2024-05-17 15:48:00 -0400

    Binary Search code

--- BinarySearchAlgorithm/src/BinarySearch.java
+++ BinarySearchAlgorithm/src/BinarySearch.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45,};
+        int target = 22;
+        int ans = binarySearch(arr,target);
+        System.out.println(ans);
+    }
+
+    //return the index
+    //return -1 if it does not exist
+    private static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            //might be possible that (start + end) exceeds the range of int in Java
+            //int middle = (start + end) / 2;
+            //Better way to find the mid
+            int middle = start + (end - start) / 2;
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                //ans found
+                return middle;
+            }
+        }
+        return -1;
+    }
 }
+
